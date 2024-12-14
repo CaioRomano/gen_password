0221cb3553f552de08855cb72220f43617b83bd4
Romano <caioprgonzaga@gmail.com>
2024-12-14 17:58:02 -0300

    v2

--- gen_password.py
+++ gen_password.py
@@ -2,17 +2,21 @@ import secrets
 import string
 
 
-def generate_password(length: int = 10) -> None:
+def generate_password(length: int = 15, special_characters: bool = False) -> None:
     """
     Função geradora de senha forte
 
     :param length: Número de caracteres presentes na senha gerada
+    :param special_characters: Se gera ou não uma senha com caracteres especiais
     """
 
-    alphabet = string.ascii_letters + string.digits
+    alphabet = string.ascii_letters + string.digits + string.punctuation \
+        if special_characters \
+        else string.ascii_letters + string.digits
+
     password = ''.join(secrets.choice(alphabet) for i in range(length))
     print(password)
 
 
 if __name__ == '__main__':
-    generate_password(25)
+    generate_password(25, True)
